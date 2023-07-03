4f0ace6cff5e3a262bb2f6e7ed92b3199b106794
OtavioLira <otavioliraneves@gmail.com>
2023-07-03 18:24:29 -0300

    Testando a primeira conexão com o banco de dados

--- src/br/com/tokio/testes/Teste.java
+++ src/br/com/tokio/testes/Teste.java
@@ -1,16 +1,16 @@
 package br.com.tokio.testes;
 
-import javax.swing.JOptionPane;
 
-import br.com.tokio.model.CriarUsuario;
+import br.com.tokio.controller.UsuarioDAO;
+import br.com.tokio.model.Usuario;
 
 public class Teste {
 
 	public static void main(String[] args) {
-		String senha = JOptionPane.showInputDialog(null,"digite uma senha");
-		
-		boolean resp = CriarUsuario.verificarSenha(senha);
-		JOptionPane.showMessageDialog(null, "A senha passou no teste: " + resp);
+		UsuarioDAO dao = new UsuarioDAO();
+		Usuario novoUsuario = new Usuario(574162138,"Arthur","Fiap@123", "11082005", "masc", 181134165);
+		dao.insert(novoUsuario);
+		System.out.println(dao.selectAll());
 	}
 
 }
